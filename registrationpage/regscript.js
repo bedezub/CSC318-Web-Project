b812b06ecd95c3e714e899726189278806818957
khunamir <amir12102009@gmail.com>
2021-01-11 16:21:18 +0800

    added login page

--- registrationpage/regscript.js
+++ registrationpage/regscript.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const studentid = document.getElementById('studentid');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
@@ -11,21 +11,20 @@ form.addEventListener('submit', e => {
 });
 
 function checkInputs() {
-	// trim to remove the whitespaces
-	const usernameValue = username.value.trim();
+
+	const studentidValue = studentid.value.trim();
 	const emailValue = email.value.trim();
 	const passwordValue = password.value.trim();
 	const password2Value = password2.value.trim();
-
-	// const passwordCheck = /^[a-zA-Z][0-9]{7}$/
-	// const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	
-	if(usernameValue === '') {
-		setErrorFor(username, 'Username cannot be blank');
+	// Check if student id is empty or not
+	if(studentidValue === '') {
+		setErrorFor(studentid, 'Student ID cannot be blank');
 	} else {
-		setSuccessFor(username);
+		setSuccessFor(studentid);
 	}
 	
+	// Check if email is empty or not and if it is a valid email
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
 	} else if(!isValidEmail(emailValue)) {
@@ -34,6 +33,7 @@ function checkInputs() {
 		setSuccessFor(email);
 	}
 	
+	// Check if password is empty or not and if it has minimum of 8 characters
 	if(passwordValue === '') {
 		setErrorFor(password, 'Password cannot be blank');
 	} else if(!isValidPassword(passwordValue)) {
@@ -43,6 +43,7 @@ function checkInputs() {
 		setSuccessFor(password);
 	}
 	
+	// Check if confirmation password is empty or not and if it is the same as previous password
 	if(password2Value === '') {
 		setErrorFor(password2, 'Confirmation password cannot be blank');
 	} else if(passwordValue !== password2Value) {
@@ -56,21 +57,27 @@ function setErrorFor(input, message) {
 	const formControl = input.parentElement;
     const small = formControl.querySelector('small');
 
+	// Display error message and icon if there is an error
     formControl.className = 'form-control error';
 	small.innerText = message;
 	formControl.style.marginBottom = "6px";
 }
 
 function setSuccessFor(input) {
+
+	// Display success message on correct input
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 	formControl.style.marginBottom = "0";
 }
 	
+// Function to check valid email using regex
 function isValidEmail(emailValue) {
 	return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(emailValue);
 }
 
+// Function to check valid password using regex, with minimum of 8 characters and must be a combination of alphabet
+// and numeric
 function isValidPassword(passwordValue) {
 	return /^((?=\S*?[a-z])(?=\S*?[0-9]).{7,})\S$/.test(passwordValue);
 }
