48e567fc218a377b84d0fda653658a7ecf6a07a1
zubquzaini <zubliquzaini@gmail.com>
2021-01-21 23:03:09 +0800

    added new sql

---
+++ sql/ci4-v3.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 21, 2021 at 04:00 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Candidates`
+--
+
+CREATE TABLE `Candidates` (
+  `candidateID` int(12) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `userpassword` varchar(80) NOT NULL,
+  `profilePicture` varchar(80) NOT NULL,
+  `userType` varchar(80) NOT NULL,
+  `faculty` varchar(80) NOT NULL,
+  `manifesto` varchar(255) NOT NULL,
+  `manifestoMedia` varchar(80) NOT NULL,
+  `mediaType` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Candidates`
+--
+
+INSERT INTO `Candidates` (`candidateID`, `email`, `username`, `userpassword`, `profilePicture`, `userType`, `faculty`, `manifesto`, `manifestoMedia`, `mediaType`) VALUES
+(111, 'ali@gmail.com', 'Muhd Ali', '12345', '111-1611022574.jpg', 'candidate', 'fskm', 'Saya akan kasi RM5\r\n- Percuma botol air milo\r\n- Ais krim chocolate\r\n- Nescafe ais\r\n- Teh tarik\r\n- Gula melaka', '111.mp4', 'video/mp4'),
+(112, 'dd', '', '', '', '', '', '', '', ''),
+(141, '1111', '', '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `studentID` int(12) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `userpassword` varchar(80) NOT NULL,
+  `isAttend` int(5) NOT NULL,
+  `userType` varchar(80) NOT NULL,
+  `faculty` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`studentID`, `email`, `username`, `userpassword`, `isAttend`, `userType`, `faculty`) VALUES
+(1111, 'admin@gmail.com', 'admin', '12345', 1, 'admin', 'fskm'),
+(2222, 'zubliquzaini@gmail.com', 'zubquzaini', '123', 0, 'voter', 'fskm'),
+(2018442458, 'voter', 'voter', '12345', 1, 'Admin', 'fskm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Vote`
+--
+
+CREATE TABLE `Vote` (
+  `voteID` int(12) NOT NULL,
+  `studentID` int(12) NOT NULL,
+  `candidateID` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Vote`
+--
+
+INSERT INTO `Vote` (`voteID`, `studentID`, `candidateID`) VALUES
+(2121151, 2222, 111);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Candidates`
+--
+ALTER TABLE `Candidates`
+  ADD PRIMARY KEY (`candidateID`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`studentID`);
+
+--
+-- Indexes for table `Vote`
+--
+ALTER TABLE `Vote`
+  ADD PRIMARY KEY (`voteID`),
+  ADD KEY `studentID` (`studentID`),
+  ADD KEY `candidateID` (`candidateID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Vote`
+--
+ALTER TABLE `Vote`
+  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `Users` (`studentID`),
+  ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`candidateID`) REFERENCES `Candidates` (`candidateID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
